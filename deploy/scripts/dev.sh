94d451f92d5061c42450c02be35ec1a434b03207
Kabir <kabir.gupta@deepmindz.co>
2025-07-08 12:46:51 +0530

    fix:dev.sh

--- deploy/scripts/dev.sh
+++ deploy/scripts/dev.sh
@@ -3,4 +3,4 @@ source ~/.nvm/nvm.sh
 rm -rf node_modules
 npm install
 pm2 delete OUTLOOK_FRONTEND_DEV
-pm2 start "npm run start:dev" -n "SMARTIN_FRONTEND_DEV"
+pm2 start "npm run start:dev" -n "OUTLOOK_FRONTEND_DEV"
